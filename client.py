99f9a0ee85633914eb52d32bdd3354bb60cff14f
Armando Rios Gallego <armandosebas4@gmail.com>
2021-08-28 14:00:43 -0500

    cleaned last push

--- client.py
+++ client.py
@@ -3,12 +3,6 @@ import json
 import threading
 
 conn = http.client.HTTPConnection("localhost",8000)
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-data1 = r1.read()
-data2 = data1
-print(data1)
 
 while True:
     conn.request("GET", "/")
@@ -19,7 +13,7 @@ while True:
         print(data1)
         data2 = data1
     headers = {'Content-type': 'application/json'}
-    message = input("Ingrese el mensaje")
+    message = input("Ingrese el mensaje\n")
     foo = {'text': f'{message}'}
     json_data = json.dumps(foo)
     conn.request('POST', '/post', json_data, headers)
